8b86d32f4f583212a04f6d0e0cc3366c622365a4
= <Neugebauer_David@web.de>
2012-11-16 22:30:03 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FsPhilo::Application.config.secret_token = 'ed4f8e905aad51edabe63f84da2074f74f1a1b50cfd5677de7ed0d522657322afb77a0f28fcab0fc0b32f55274c9aa0a982cecde82e4a354a325987287e229ed'
