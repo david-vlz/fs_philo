5d29a05c94a1e1c3a0d9180e668d52f0ce067b33
= <Neugebauer_David@web.de>
2012-11-18 13:15:57 +0100

    add veranstaltungen route and controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ FsPhilo::Application.routes.draw do
   
   root to: 'static_pages#home'
   
-  match 'contact', to: 'static_pages#contact'
-  
-  get "static_pages/home"
-
-  get "static_pages/contact"
+  match 'contact', 			to: 'static_pages#contact'
+  match 'veranstaltungen', 	to: 'static_pages#veranstaltungen'
 
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,15 @@ require 'spec_helper'
 
 describe "Static pages" do
   
-  subject { page }
+	subject { page }
+  
+	shared_examples_for "all static pages" do
+		it { should have_selector('h1', text: 'Fachschaft Philosophie') }
+	end
+	
+	describe 'Home Page' do
+		before { visit root_path }
+		it_should_behave_like 'all static pages'
+	end
   
 end
