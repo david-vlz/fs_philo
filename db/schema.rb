834c6dffb1bab75ce4c505960786d3c827e0a8f8
= <Neugebauer_David@web.de>
2012-11-18 19:45:12 +0100

    add user model and tests (import from sample app project)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121118180630) do
+ActiveRecord::Schema.define(:version => 20121118183825) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(:version => 20121118180630) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "fs_user_id"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
