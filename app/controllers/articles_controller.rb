56e64d1d7080a6f60a15686bc273473c18cfa408
= <Neugebauer_David@web.de>
2012-12-24 02:05:42 +0100

    implement links and routes to articles.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,10 @@
 class ArticlesController < ApplicationController
-  def new
-  end
+
+	def show
+		@categories = getCategories
+		@article = Article.find(params[:id])
+	end
+
+	def new
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,10 @@
 FsPhilo::Application.routes.draw do
-  
-#  get "categories/show"
 
-#  get "categories/new"
-
-#  get "articles/new"
-
-  resources :categories
+  resources :categories, :articles
 
   root to: 'static_pages#home'
   
   match 'contact', 			to: 'static_pages#contact'
   match 'veranstaltungen', 	to: 'static_pages#veranstaltungen'
-  
-  match 'cats', 			to: 'categories'
 
 end
