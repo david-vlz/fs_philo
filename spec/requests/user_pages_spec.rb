cf6ff65fd1ffc443f27c0633cecea5c02100c5a1
= <Neugebauer_David@web.de>
2012-12-26 18:32:37 +0100

    tests for user and user signup pages

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -24,13 +24,37 @@ describe "User pages" do
 		
 		before { visit anmeldung_path }
 		
+		describe "basic page layout" do
+			it { should have_selector('label[for="user_name"]', text: 'Name') }
+			it { should have_selector('input[id="user_name"]') }
+			it { should have_selector('label[for="user_email"]', text: 'Email') }
+			it { should have_selector('input[id="user_email"]') }
+			it { should have_selector('label[for="user_password"]', text: 'Passwort') }
+			it { should have_selector('input[id="user_password"]') }
+			it { should have_selector('label[for="user_password_confirmation"]', text: 'Passwort bestätigen') }
+			it { should have_selector('input[id="user_password_confirmation"]') }
+			it { should have_selector('input[class="btn btn-primary"][name="commit"][type="submit"][value="Account erstellen"]') }
+			
+		end
+		
+		
 		let(:submit) { "Account erstellen" }
 		
 		describe "with invalid information" do
+			
 			it "should not create an account" do
 				expect { click_button submit }.not_to change(User, :count)
 			end
+			
+			describe "after submission" do 
+				before { click_button submit }
+				it { should have_selector('title', text: 'Anmelden') }
+				it { should have_content('8 Fehler gefunden') }
+				it { should have_selector('div[class="field_with_errors"]') }
+			end
 		end
+		
+		
 		
 		describe "with valid information" do
 			before do
@@ -42,6 +66,17 @@ describe "User pages" do
 
 			it "should create a user" do
 				expect { click_button submit }.to change(User, :count).by(1)
+			end
+			
+			describe "after saving the user" do
+				before { click_button submit }
+				let(:user) { User.find_by_email('ssd@example.com') } # spec/factories.rb
+				
+				it { should have_selector('title', text: "User #{user.name} |") }
+				it { should have_selector('div.alert.alert-success', text: 'Willkommen bei der Fachschaft!') }
+				it { should have_selector('p', text: user.email) }
+				it { should have_selector('i.icon-user') }
+				it { should have_selector('h4', text: 'Articles') }
 			end
 		end
 		
