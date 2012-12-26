cebbae5c320aa5f81a44dba6d0871e9979ad677e
= <Neugebauer_David@web.de>
2012-12-26 19:35:07 +0100

    basic tests and layout for login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,13 @@
 class SessionsController < ApplicationController
+
+	def new
+		@categories = Category.all
+	end
+	
+	def create
+	end
+	
+	def destroy
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 FsPhilo::Application.routes.draw do
 
   resources :categories, :articles, :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root to: 'static_pages#home'
   
-  match 'anmeldung', to: 'users#new'
+  match '/anmeldung', to: 'users#new'
+  match '/login', to: 'sessions#new'
+  match '/logout', to: 'sessions#destroy', via: :delete
 
 end
