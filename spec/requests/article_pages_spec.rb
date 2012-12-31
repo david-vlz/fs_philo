fe10a2b62f04d0557238e507ca8f43af93802056
= <Neugebauer_David@web.de>
2012-12-31 14:45:31 +0100

    tests for the basic page layout of category overviews

---
+++ spec/requests/article_pages_spec.rb
@@ -0,0 +1,22 @@
+# coding : utf-8
+
+require 'spec_helper'
+
+describe 'Article Pages' do
+	
+	describe 'when creating an article' do
+		
+		before { visit new_article_path }
+		
+		describe 'the basic page layout' do
+			it { should have_selector('title', text: 'Beitrag erstellen | ') }
+			it { should have_selector('h3', text: 'Beitrag erstellen') }
+			it { should have_selector('div.short-info', text: 'Jeder Beitrag ist in Abschnitte gegliedert') }
+			it { should have_selector('i.icon-info-sign') }
+			it { should have_selector('h4', text: 'Abschnitt: 1') }
+			it { should have_button('Neuen Abschnitt hinzufügen') }
+		end
+		
+	end
+	
+end
