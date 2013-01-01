d8ae2769795f02ac7b9d11e45876fb6d3ff17e6a
= <Neugebauer_David@web.de>
2013-01-01 22:39:27 +0100

    add new category link to header when signed in

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -20,7 +20,7 @@ describe "Static pages" do
 			it { should have_selector('h1', text: 'Fachschaft Philosophie') }
 			it { should have_link('Home', href: root_path) }
 			it { should have_link(category.name, href: category_path(category)) }
-			it { should have_link('Seite hinzufügen', href: new_category_path) }
+			it { should have_link('Neue Seite anlegen', href: new_category_path) }
 			it { should have_link('Logout', href: logout_path) }
 			it { should have_link('Profil', href: user_path(user)) }
 			it { should have_link('Users', href: users_path) }
@@ -30,6 +30,7 @@ describe "Static pages" do
 			it { should have_link('Login', href: login_path) }
 			it { should_not have_link('Profil', href: user_path(user)) }
 			it { should_not have_link('Users', href: users_path) }
+			it { should_not have_link('Neue Seite anlegen', href: new_category_path) }
 		end
 	end
 	
