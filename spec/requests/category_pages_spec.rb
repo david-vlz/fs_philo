7c6487e21da3ae7060a41f300056fcf98329d071
= <Neugebauer_David@web.de>
2013-01-03 12:01:36 +0100

    Refactored tests for categories pages.

--- spec/requests/category_pages_spec.rb
+++ spec/requests/category_pages_spec.rb
@@ -9,16 +9,16 @@ describe "Category pages" do
 	describe "with multiple articles" do
 		let(:category) { FactoryGirl.create(:category) }
 		let(:user) { FactoryGirl.create(:user) }
-		let(:articles_amount) { 4 }
 		before do 
-			articles_amount.times { |n| category.articles.create!(title: "title #{n}", body: "Lorem Ipsum", active: true, user_id: user.id) } 
+			3.times { |n| category.articles.create!(title: "title #{n}", body: "Lorem Ipsum #{n}", active: true, user_id: user.id) } 
 			visit category_path(category)
 		end
 		let(:article) { category.articles.first }
-
-		it { should have_selector('h3', text: "title ", count: articles_amount) }
-		it { should have_selector('p.lead', text: "Lorem Ipsum", count: articles_amount) }
-		it { should have_selector('h3', text: article.title) }
+		
+		3.times do |n|
+			it { should have_selector('h3', text: "title #{n}") }
+			it { should have_selector('p.lead', text: "Lorem Ipsum #{n}") }
+		end
 		it { should have_link(article.title, href: article_path(article)) }
 		it { should have_link('Artikel hinzufügen', href: new_article_path) }
 	end
