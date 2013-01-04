42edb7168172f65818cfbac97fa4af560adf5801
= <Neugebauer_David@web.de>
2013-01-04 23:52:21 +0100

    Added routes for versions of articles.

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,12 @@ FsPhilo::Application.routes.draw do
 
   root to: 'static_pages#home'
   
-  match '/anmeldung', to: 'users#new'
-  match '/login', to: 'sessions#new'
-  match '/logout', to: 'sessions#destroy', via: :delete
+  match '/anmeldung', 	to: 'users#new'
+  match '/login', 		to: 'sessions#new'
+  match '/logout', 		to: 'sessions#destroy', via: :delete
+  
+  match 'versions/articles/:id/revert',	to: 'versions#revert', 	via: :post,	as: 'revert_version'
+  match 'versions/articles/:id',		to: 'versions#list', 	via: :get, 	as: 'versions'
+  match 'versions/:id', 				to: 'versions#show', 	via: :get, 	as: 'version'
 
 end
