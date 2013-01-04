96a5b33a68f83fdb3185b5a84289383709b83a88
= <Neugebauer_David@web.de>
2013-01-04 23:19:53 +0100

    Refactored articles controller to use a before filter like all the other controllers in his class do.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,25 +60,4 @@ class UsersController < ApplicationController
 		end
 	end
 	
-	
-	
-	private
-	
-		def signed_in_user
-			if not signed_in?
-				store_location
-				flash[:notice] = "Bitte loggen sie sich ein, um diese Funktion zu nutzen."
-				redirect_to login_url
-			end
-		end
-		
-		def correct_user
-			@user = User.find_by_id(params[:id])
-			redirect_to root_path unless current_user?(@user)
-		end
-		
-		def admin_user
-			redirect_to root_path unless current_user.admin?
-		end
-	
 end
