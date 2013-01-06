cfafddef6b7762796f923916995eee84f66f679e
= <Neugebauer_David@web.de>
2013-01-06 20:51:16 +0100

    Added links to versions list, implemented display of article versions.

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -1,9 +1,11 @@
 class VersionsController < ApplicationController
 	
+	include VersionsHelper
 	before_filter :signed_in_user
 	
 	def show
 		@version = Version.find_by_id(params[:id])
+		@article = @version.reify
 	end
 
 	def list
--- app/helpers/versions_helper.rb
+++ app/helpers/versions_helper.rb
@@ -1,2 +1,18 @@
 module VersionsHelper
+	
+	def whodunnit_string(version)
+		uid = version.whodunnit || '?'
+		user = User.find_by_id(uid.to_i) if '?' != uid
+		user ? user.name : uid
+	end
+	
+	def whodunnit_user(version)
+		uid = version.whodunnit
+		uid ? User.find_by_id(uid.to_i) : nil
+	end
+	
+	def string_from_time(time)
+		time.strftime("%d %b. %Y, %H:%M")
+	end
+
 end
