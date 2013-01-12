660ac88069fc51505bd632b7bd59e830995084ed
= <Neugebauer_David@web.de>
2013-01-12 10:10:40 +0100

    Changed assets precompilation option configuratin to allow rake assets precompile on heroku.

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,10 @@ module FsPhilo
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # fixes rake assets:precompile error on heroku
+    # http://www.simonecarletti.com/blog/2012/02/heroku-and-rails-3-2-assetprecompile-error/
+    config.assets.initialize_on_precompile = false
+    
   end
 end
